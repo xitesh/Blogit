80c4a1b08167e2e2b185205b6e6558639c28a507
Nitesh Nagar <itsniteshnagar@gmail.com>
2025-05-24 16:38:40 +0000

    User Registration Connection with backend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const register = async (e) => {
+    e.preventDefault();
+    await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type': 'application/json'},
+    })
+  }
   return (
-    <form className="register">
+    <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+      <input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
       <button>Register</button>
     </form>
   );
